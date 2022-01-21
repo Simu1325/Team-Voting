6d8f9aa57278d83d5bb36309f975e74149a1fbbf
Simu1325 <82212011+Simu1325@users.noreply.github.com>
2022-01-21 21:15:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,23 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnKRvJMUdA71mfhZVpaj73A4QXJNqC9X4",
+  authDomain: "voting-7e141.firebaseapp.com",
+  projectId: "voting-7e141",
+  storageBucket: "voting-7e141.appspot.com",
+  messagingSenderId: "110322642228",
+  appId: "1:110322642228:web:5793733ad0409fb1984961"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
